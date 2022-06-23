ef4bd0d2b4fbb66f706bb4484c07e4a292075aed
tonayam <tonayam99@gmail.com>
2022-06-23 05:47:49 +0100

    Updates

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const toggleMenu = document.querySelector(`.bi-list`);
+const toggleMenu = document.querySelector(`.toggleBar`);
 const closeMenu = document.querySelector(`.bi-x-lg`);
 const menuItems = document.querySelector(`.mob-nav-items`);
+const container = document.querySelector(`.nav-cont`);
+console.log(container);
 
 toggleMenu.addEventListener(`click`, function () {
   menuItems.classList.add(`show-nav`);
   closeMenu.classList.add(`show-x`);
   toggleMenu.style.display = `none`;
+  container.style.background = `#ebfbfb`;
 });
 
 closeMenu.addEventListener(`click`, function () {
   menuItems.classList.remove(`show-nav`);
   closeMenu.classList.remove(`show-x`);
   toggleMenu.style.display = `block`;
+  container.style.background = `white`;
 });
